8b4e0972beec8c30eabc0ac469ed4a09b2d6bfb9
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2024-04-12 12:36:48 +0300

    2.6-2.8

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <form>
+         <div>name: <input /></div>
+       <div>number: <input /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+      </div>
+  )
+
+}
+
+export default App
