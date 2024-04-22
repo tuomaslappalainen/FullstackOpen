306d3b55e8c062520ea4c25684f93425902d8a49
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2024-04-22 13:52:27 +0300

    2.9-2.17

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,29 +1,130 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import dataService from './Services/persons'
 
+const FilterForm = (props) => {
+  console.log(props)
+  return (
+    <div>
+      filter shown with: <input onChange={props.handleFilterChange}/>
+    </div>
+  )
+}
+const Person = (props) => {
+  console.log(props)
+  return (
+    <div>
+      {props.person.name} {props.person.number}
+      <input type='button' value='delete' onClick={() => {
+        console.log(props.person.id)
+        axios.delete(`http://localhost:3001/persons/${props.person.id}`).then(response => {
+          console.log(response)
+          window.location.reload()
+        })
+      }}/>
+    </div>
+  )
+}
+
+const People = (props) => {
+  console.log(props)
+  return (
+    <div>
+      {props.list.filter(person => person.name.toLowerCase().includes(props.filter.toLowerCase())).map(person =>
+        <Person key={person.name} person={person}/>
+      )}
+    </div>
+  )
+}
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <p>{message}</p>
+  )
+}
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([])
+  const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    console.log('effect')
+    dataService.getAll().then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+      console.log(persons)
+    })
+  }, [])
+
+  const addPerson = (e) => {
+    e.preventDefault()
+
+    const names = persons.map(p => {
+      return p.name
+    })
+    if(names.includes(newName)) {
+      document.querySelector('.notification').style = 'color: red'
+      setNotification(`${newName} is already added`)
+      setTimeout(() => {setNotification(null)}, 5000)
+      return
+    }
+
+    const person = {
+      name: newName,
+      number: newNumber
+    }
+
+    dataService.create(person).then(response => {
+      console.log(response)
+
+      document.querySelector('.notification').style = 'color: green'
+      setNotification(`${person.name} was added to the server`)
+      setTimeout(() => {setNotification(null)}, 5000)
+
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
+  }
+
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+  const handleNumChange = (e) => {
+    setNewNumber(e.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <div className='notification'>
+        <Notification message={notification}/>
+      </div>
+      <FilterForm handleFilterChange={handleFilterChange}/>
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <form>
-         <div>name: <input /></div>
-       <div>number: <input /></div>
-      <div><button type="submit">add</button></div>
-    </form>
-      </div>
+      <People list={persons} filter={filter}/>
+    </div>
   )
 
 }
--- osa2/puhelinluettelo/src/main.jsx
+++ osa2/puhelinluettelo/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import axios from 'axios'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App'
+
+const promise = axios.get('http://localhost:3001/db')
+console.log(promise)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
