d16d6cb71b07077ad2414ad135ba09a62f3b508d
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2024-05-03 14:16:02 +0300

    2.18-2.20

---
+++ osa2/maidentiedot/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import getAll from "./services/restCountries";
+import FilterCountries from "./components/FilterCountries";
+import Countries from "./components/Country";
+
+function App() {
+  const [newSearch, setNewSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  const handleSearchChange = event => {
+    setNewSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    getAll().then(response => setCountries(response));
+  }, []);
+
+  return (
+    <div>
+      <FilterCountries
+        newSearch={newSearch}
+        handleSearchChange={handleSearchChange}
+      />
+      <Countries countries={countries} newSearch={newSearch} />
+    </div>
+  );
+}
+export default App;
