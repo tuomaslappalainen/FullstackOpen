f6593024378f90db511b12afb9415326ff57194f
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2024-08-30 13:28:11 +0300

    Commit

---
+++ osa6/unicafe-redux/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+
+    return (
+      <div>
+        <NewNote />
+        <Notes />
+      </div>
+    )
+  }
+
+  export default App
--- osa6/unicafe-redux/src/main.jsx
+++ osa6/unicafe-redux/src/main.jsx
@@ -1,35 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { createStore } from 'redux'
-import reducer from './noteReducer'
+import { Provider } from 'react-redux'
 
+import App from './App'
+import noteReducer from './reducers/noteReducer'
 
-const store = createStore(reducer)
+const store = createStore(noteReducer)
 
-const App = () => {
-  const handleClick = (type) => {
-    store.dispatch({ type })
-  };
-
-  return (
-    <div>
-      <button onClick={() => handleClick("GOOD")}>good</button> 
-      <button onClick={() => handleClick("OK")}>ok</button> 
-      <button onClick={() => handleClick("BAD")}>bad</button>
-      <button onClick={() => handleClick("ZERO")}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
